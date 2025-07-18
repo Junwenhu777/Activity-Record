710df51ee1c1100fc9e08458c65fb4d9578e79b0
Junwenhu777 <135290890+Junwenhu777@users.noreply.github.com>
2025-07-18 09:38:44 -0300

    feat: 完善PWA配置

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import 'antd-mobile/es/global'
-
+import { registerSW } from 'virtual:pwa-register';
+registerSW();
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Activity Record',
+        short_name: 'Activity',
+        start_url: '.',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#00313c',
+        description: '移动端活动记录PWA应用',
+        icons: [
+          {
+            src: '/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
